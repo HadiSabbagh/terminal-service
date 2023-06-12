d61ed16629c343625d63d468d0e04c731d539279
Hadi Sabbagh <hadi_sabbagh02@outlook.com>
2023-06-12 22:23:12 +0300

    Added terminal-service.

--- src/main/java/org/toyota/dao/TerminalRepository.java
+++ src/main/java/org/toyota/dao/TerminalRepository.java
@@ -2,7 +2,6 @@ package org.toyota.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.toyota.domain.Data;
 import org.toyota.domain.Terminal;
 
 @Repository
--- src/main/java/org/toyota/resources/TerminalServiceController.java
+++ src/main/java/org/toyota/resources/TerminalServiceController.java
@@ -1,16 +1,10 @@
 package org.toyota.resources;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.toyota.dao.TerminalRepository;
-import org.toyota.domain.Data;
-
-import java.io.File;
-import java.io.IOException;
 
 
 @RestController
@@ -22,17 +16,13 @@ public class TerminalServiceController
     @GetMapping("/getTerminal")
     public ResponseEntity<Object> getTerminal()
     {
-      String json = "src/main/resources/terminals.json";
-      ObjectMapper objectMapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        try
+        if(!terminalRepository.findAll().isEmpty())
         {
-            Data data = objectMapper.readValue(new File("G:\\NewStart\\Projects\\terminal-service\\src\\main\\resources\\terminals.json"), Data.class);
-            terminalRepository.saveAll(data.getTerminals());
             return ResponseEntity.ok().body(terminalRepository.findAll());
-        } catch (IOException e)
-        {
-            throw new RuntimeException(e);
         }
-
+        else
+        {
+            return ResponseEntity.badRequest().body("No terminals found!");
+        }
     }
 }
